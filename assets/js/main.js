a719ce573d9bfe077032d1a5299d32e5ddc930ca
GR <glennritchey@proton.me>
2024-12-08 15:16:01 -0500

    Update and rename script.js to main.js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+function toggleMobileMenu() {
+    const navbarMobile = document.querySelector('.navbar-mobile');
+    navbarMobile.style.display = navbarMobile.style.display === 'flex' ? 'none' : 'flex';
+}
