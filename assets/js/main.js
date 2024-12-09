e1d4bcfb54086729007502c38cb50bda6fbed70e
GR <glennritchey@proton.me>
2024-12-09 16:36:53 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 function toggleMobileMenu() {
     const navbarMobile = document.querySelector('.navbar-mobile');
-    navbarMobile.style.display = navbarMobile.style.display === 'flex' ? 'none' : 'flex';
+    navbarMobile.classList.toggle('active');
 }
