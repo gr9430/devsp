ad4d344543cdf05485440a661ccf650983d8e638
GR <glennritchey@proton.me>
2024-12-09 21:07:15 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,24 @@ function toggleMobileMenu() {
     const isActive = navbarMobile.classList.toggle('active'); // Toggles the 'active' class
     hamburger.setAttribute('aria-expanded', isActive); // Updates the aria-expanded attribute
 }
+
+// JavaScript to load the navbar on each page
+document.addEventListener("DOMContentLoaded", () => {
+    const navbarPlaceholder = document.querySelector('#navbar-placeholder');
+
+    if (navbarPlaceholder) {
+        fetch('assets/html/navbar.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Navbar could not be loaded');
+                }
+                return response.text();
+            })
+            .then(navbarHTML => {
+                navbarPlaceholder.innerHTML = navbarHTML;
+            })
+            .catch(error => {
+                console.error('Error loading the navbar:', error);
+            });
+    }
+});
